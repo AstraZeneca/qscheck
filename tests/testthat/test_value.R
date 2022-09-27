c76f7121e25b90bda37790fc80d59125e3114f11
Stefano Borini <stefano.borini@astrazeneca.com>
2022-09-27 16:37:56 +0100

    Added is_value as initial step towards passthrough

--- tests/testthat/test_value.R
+++ tests/testthat/test_value.R
@@ -1,3 +1,13 @@
+test_that("is_value", {
+  expect_true(is_value(34))
+  expect_false(is_value(NA))
+  expect_false(is_value(NULL))
+  expect_true(is_value(NA, allow_na = TRUE))
+  expect_true(is_value(NULL, allow_null = TRUE))
+  expect_false(is_value(c(34, 34)))
+
+})
+
 test_that("is_na_value", {
   expect_true(is_na_value(NA))
   expect_false(is_na_value(c(NA, NA)))
