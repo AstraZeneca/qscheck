fde3db9a44db04e177765e30e9ed253b48fbf966
Stefano Borini <stefano.borini@astrazeneca.com>
2022-11-28 17:11:54 +0000

    Changed the order, although it is eventually going away

--- R/data_frame.R
+++ R/data_frame.R
@@ -35,12 +35,12 @@ is_data_frame <- function(df,
                           allow_na = FALSE,
                           allow_null = FALSE) {
 
-  if (is_na_value(df)) {
-    return(allow_na)
-  }
-
   if (is.null(df)) {
     return(allow_null)
+  }
+
+  if (is_na_value(df)) {
+    return(allow_na)
   }
 
   if (!is.data.frame(df)) {
