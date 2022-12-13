68856f8697d1574fd5a33f0f106553d37c9daa20
Stefano Borini <stefano.borini@astrazeneca.com>
2022-12-13 18:41:08 +0000

    Added motivation to is_logical_value

--- tests/testthat/test_logical.R
+++ tests/testthat/test_logical.R
@@ -2,15 +2,9 @@ test_that("is_logical_value", {
   expect_true(is_logical_value(FALSE))
   expect_false(is_logical_value(c(FALSE, FALSE)))
   expect_false(is_logical_value(""))
-  foo <- -1.5
-  err <- tryCatch({
-      assertthat::assert_that(is_logical_value(foo))
-    },
-    error = function(e) {
-      return(e)
-    })
 
-  expect_equal(
-    as.character(err),
-    "Error: foo must be a logical value. Got: -1.5\n")
+  foo <- 1.5
+  expect_error(
+    assertthat::assert_that(is_logical_value(foo)),
+    "foo must be a logical value. Passed value is not a logical")
 })
