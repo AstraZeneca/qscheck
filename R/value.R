01ec73c5ffde640ebbc7e6a2489cb5fa93a46726
Dimitris Karletsos <46774645+dkarletsos@users.noreply.github.com>
2023-03-30 08:55:13 +0100

    less than

--- R/value.R
+++ R/value.R
@@ -243,3 +243,137 @@ inspect_greater_than_value <- function(
 
   return(success())
 }
+
+#' Check if the passed entity is a single floating point which is (strictly)
+#' less than another specified value.
+#'
+#' @param value the value to check
+#' @param comparator the maximum allowed value, inclusive or exclusive.
+#' @param strictly If TRUE (default), the comparator value is checked exclusive
+#' \code{(<)}. If FALSE the comparator value is checked inclusive \code{(<=)}.
+#'
+#' @param allow_na if true, accept a value that is NA.
+#' @param allow_null if true, accept a value that is NULL.
+#'
+#' @examples
+#' \dontrun{
+#' # For assertion
+#' assertthat::assert_that(qscheck::is_less_than_value(value, comparator))
+#' # For check
+#' if (qscheck::is_less_than_value(value, comparator)) {}
+#' }
+#'
+#' @concept value
+#' @export
+is_less_than_value <- function(value,
+    comparator, strictly = TRUE,
+    allow_na = FALSE, allow_null = FALSE) {
+
+  res <- inspect_greater_than_value(
+    value, comparator,
+    strictly = strictly,
+    allow_na = allow_na,
+    allow_null = allow_null
+  )
+
+  return(res$valid)
+}
+assertthat::on_failure(is_less_than_value) <- function(call, env) {
+  value <- callget(call, env, "value", NULL)
+  comparator <- callget(call, env, "comparator", NULL)
+  strictly <- callget(call, env, "strictly", TRUE)
+  allow_na <- callget(call, env, "allow_na", FALSE)
+  allow_null <- callget(call, env, "allow_null", FALSE)
+
+  res <- inspect_greater_than_value(
+    value, comparator,
+    strictly = strictly,
+    allow_na = allow_na,
+    allow_null = allow_null
+  )
+
+if (is.null(value) || is.null(comparator)) {
+  return(paste(
+    res$reason
+  ))
+} else {
+  return(paste0(
+    deparse(call$value),
+    snippet_must_be(paste0("smaller value than ", deparse(call$comparator))),
+    snippet_na(allow_na),
+    snippet_null(allow_null),
+    ". ", res$reason
+  ))
+}
+}
+inspect_greater_than_value <- function(
+  value, comparator, strictly = TRUE,
+  allow_na = FALSE, allow_null = FALSE) {
+
+  if (is.null(value)) {
+    if (allow_null == TRUE) {
+      return(success())
+    } else {
+      return(failure("Passed value is NULL"))
+    }
+  }
+
+  if (is.null(comparator)) {
+    return(failure("Passed comparator value is NULL"))
+  }
+
+  if (!is.numeric(value)) {
+    return(failure("Passed value is not a numerical"))
+  }
+
+  if (!is.numeric(comparator)) {
+    return(failure("Passed comparator value is not a numerical"))
+  }
+
+  if (length(value) != 1) {
+    return(failure(
+      "Passed value must be a single numerical value, not a vector"
+    ))
+  }
+
+  if (length(comparator) != 1) {
+    return(failure(
+      "Passed comparator value must be a single numerical value, not a vector"
+    ))
+  }
+
+  if (is_na_value(value)) {
+    if (allow_na == TRUE) {
+      return(success())
+    } else {
+      return(failure("Passed value was NA"))
+    }
+  }
+
+  if (is_na_value(comparator)) {
+    return(failure("Passed comparator value was NA"))
+  }
+
+  if (!is.null(comparator)) {
+    if (strictly) {
+      if (value >= comparator) {
+        return(failure(
+          paste0(
+            "Passed value ", value, " is above or equal to the maximum of ", comparator)
+          )
+        )
+      }
+    } else {
+      if (value > comparator) {
+        return(failure(
+          paste0(
+            "Passed value ", value,
+            " is below the maximum of ", comparator)
+          )
+        )
+      }
+    }
+  }
+
+  return(success())
+}
--- tests/testthat/test_value.R
+++ tests/testthat/test_value.R
@@ -39,5 +39,25 @@ test_that("greater_than_value", {
     assertthat::assert_that(is_greater_than_value(5, NULL, allow_null = TRUE)),
     "Passed comparator value is NULL"
   )
+})
 
+test_that("less_than_value", {
+  expect_true(is_less_than_value(5, 6))
+  expect_true(is_less_than_value(-5, 6))
+  expect_true(is_less_than_value(6, 6, strictly = FALSE))
+  expect_false(is_less_than_value(6, 6, strictly = TRUE))
+  expect_false(is_less_than_value(6, 5))
+  expect_false(is_less_than_value(5, -6))
+  expect_error(
+    assertthat::assert_that(is_less_than_value("hello", 5, allow_na = TRUE)),
+    ""hello" must be a smaller value than 5 or NA. Passed value is not a numerical"
+  )
+  expect_error(
+    assertthat::assert_that(is_less_than_value(value = NULL, 5)),
+    "Passed value is NULL"
+  )
+  expect_error(
+    assertthat::assert_that(is_less_than_value(5, NULL, allow_null = TRUE)),
+    "Passed comparator value is NULL"
+  )
 })
