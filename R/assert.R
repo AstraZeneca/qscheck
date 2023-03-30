127d01c4d7c05502637d9406ebe1cf5d79e508d7
Stefano Borini <stefano.borini@astrazeneca.com>
2023-03-30 14:43:08 +0100

    Updated documentation

--- R/assert.R
+++ R/assert.R
@@ -1,6 +1,8 @@
 #' Shorter version of assertthat::assert_that
 #'
 #' @param \dots Passthrough to assertthat::assert_that
+#' @param env environment on which to apply the assertion
+#' @param msg overrides the error message, if specified.
 #'
 #' @export
 assert <- function(..., env = parent.frame(), msg = NULL) {
@@ -10,6 +12,8 @@ assert <- function(..., env = parent.frame(), msg = NULL) {
 #' Returns just the error message or NULL if no error.
 #'
 #' @param \dots Condition to test
+#' @param env environment on which to apply the assertion
+#' @param msg overrides the error message, if specified.
 #'
 #' @export
 message <- function(..., env = parent.frame(), msg = NULL) {
