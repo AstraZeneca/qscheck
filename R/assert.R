bbb03186be0ab290182cb42fdf7e6e76d3d4e755
Stefano Borini <stefano.borini@astrazeneca.com>
2023-03-30 14:40:08 +0100

    QSOL-2753 obtain error message without assertion

--- R/assert.R
+++ R/assert.R
@@ -6,3 +6,16 @@
 assert <- function(..., env = parent.frame(), msg = NULL) {
   assertthat::assert_that(..., env = env, msg = msg)
 }
+
+#' Returns just the error message or NULL if no error.
+#'
+#' @param \dots Condition to test
+#'
+#' @export
+message <- function(..., env = parent.frame(), msg = NULL) {
+  res <- assertthat::validate_that(..., env = env, msg = msg)
+  if (res == TRUE) {
+    return(NULL)
+  }
+  return(res)
+}
