a6e7255af9d62fdf0861f4f24942ea092b75bbac
Stefano Borini <stefano.borini@astrazeneca.com>
2023-04-03 14:46:39 +0100

    Exported success and failure functions

--- R/obj-report.R
+++ R/obj-report.R
@@ -1,7 +1,18 @@
+#' Function to create a failure. Returns a qscheck::report object.
+#'
+#' @param reason A string explaining the reason for the failure to the user.
+#' @param tag A unique tag to identify the failure
+#' @param subreport if the failure arises from a subcheck, this will contain
+#'        the report of that subcheck
+#'
+#' @export
 failure <- function(reason = NULL, tag = NULL, subreport = NULL) {
   return(report(FALSE, reason, tag, subreport))
 }
 
+#' Function to create a success. Returns a qscheck::report object.
+#'
+#' @export
 success <- function() {
   return(report(TRUE, NULL, NULL))
 }
